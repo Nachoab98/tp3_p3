59017d47e6cdc5d16cab51493135c5de00bbfdc6
user <emasuarez12@gmail.com>
2024-11-11 22:31:26 -0300

    ventanaAdjudicación

--- Tp_3/src/view/VentanaAdjudicacion.java
+++ Tp_3/src/view/VentanaAdjudicacion.java
@@ -67,68 +67,42 @@ public class VentanaAdjudicacion extends JPanel {
 		
 		JPanel panelInferiorIzquierda = new JPanel();
 		panelInferior.add(panelInferiorIzquierda);
-		panelInferiorIzquierda.setLayout(new GridLayout(0, 1, 0, 0));
+		panelInferiorIzquierda.setLayout(new BorderLayout(0, 0));
 		
 		JLabel label_heuristica = new JLabel("Heuristica:");
-		label_heuristica.setFont(new Font("Sitka Subheading", Font.BOLD, 14));
+		label_heuristica.setFont(new Font("Tahoma", Font.BOLD, 12));
 		label_heuristica.setHorizontalAlignment(SwingConstants.CENTER);
-		panelInferiorIzquierda.add(label_heuristica);
+		panelInferiorIzquierda.add(label_heuristica, BorderLayout.NORTH);
+		
+		JPanel panel_1_1 = new JPanel();
+		panelInferiorIzquierda.add(panel_1_1, BorderLayout.WEST);
+		panel_1_1.setPreferredSize(new Dimension(45,0));
+		
+		JPanel panel_1_2 = new JPanel();
+		panelInferiorIzquierda.add(panel_1_2, BorderLayout.EAST);
+		panel_1_2.setPreferredSize(new Dimension(45,0));
+		
+		JLabel label_gananciaTotal = new JLabel("Ganancia Total: ");
+		label_gananciaTotal.setFont(new Font("Tahoma", Font.BOLD, 12));
+		label_gananciaTotal.setHorizontalAlignment(SwingConstants.CENTER);
+		panelInferiorIzquierda.add(label_gananciaTotal, BorderLayout.SOUTH);
+		
+		JPanel panel_4 = new JPanel();
+		panelInferiorIzquierda.add(panel_4, BorderLayout.CENTER);
+		panel_4.setLayout(new GridLayout(3, 0, 0, 0));
 		
 		JComboBox<String> cmbx_heuristica = new JComboBox<String>();
+		panel_4.add(cmbx_heuristica);
 		cmbx_heuristica.addItem("Según precio total de reserva");
 		cmbx_heuristica.addItem("Según relación precio/Hora");
 		cmbx_heuristica.addItem("OPT (Mejor selección posible de ofertas)");
 		
-		panelInferiorIzquierda.add(cmbx_heuristica);
-		
-		JPanel panel_1_1 = new JPanel();
-		panelInferiorIzquierda.add(panel_1_1);
+		JPanel panel_5 = new JPanel();
+		panel_4.add(panel_5);
 		
 		JButton botonCalcular_1 = new JButton("Calcular Adjudicacion");
-		botonCalcular_1.setFont(new Font("Sitka Subheading", Font.BOLD, 13));
-		panelInferiorIzquierda.add(botonCalcular_1);
-		
-		JPanel panel_1_2 = new JPanel();
-		panelInferiorIzquierda.add(panel_1_2);
-		
-		JLabel label_gananciaTotal = new JLabel("Ganancia Total: ");
-		label_gananciaTotal.setFont(new Font("Sitka Subheading", Font.BOLD, 14));
-		label_gananciaTotal.setHorizontalAlignment(SwingConstants.LEFT);
-		panelInferiorIzquierda.add(label_gananciaTotal);
-
-		JPanel panelInferiorDerecha = new JPanel();
-		panelInferior.add(panelInferiorDerecha);
-		
-		JButton botonMenu = new JButton("Volver al Menu");
-		botonMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				pantallaPrincipal.cambiarVentana("Ventana Menu");
-			}
-		});
-		panelInferiorDerecha.setLayout(new BorderLayout(0, 0));
-		panelInferiorDerecha.add(botonMenu);
-		
-		JPanel panel_1 = new JPanel();
-		panelInferiorDerecha.add(panel_1, BorderLayout.NORTH);
-		panel_1.setPreferredSize(new Dimension(0,50));
-		
-		JPanel panel_2 = new JPanel();
-		panelInferiorDerecha.add(panel_2, BorderLayout.SOUTH);
-		panel_2.setPreferredSize(new Dimension(0,50));
-		
-		JPanel panel = new JPanel();
-		panelInferiorDerecha.add(panel, BorderLayout.WEST);
-		panel.setPreferredSize(new Dimension(50,0));
-		
-		JPanel panel_3 = new JPanel();
-		panelInferiorDerecha.add(panel_3, BorderLayout.EAST);
-		panel_3.setPreferredSize(new Dimension(50,0));
-
-		JPanel panelIzquierda = new JPanel();
-		add(panelIzquierda, BorderLayout.WEST);
-
-		JPanel panelDerecha = new JPanel();
-		add(panelDerecha, BorderLayout.EAST);
+		panel_4.add(botonCalcular_1);
+		botonCalcular_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		
 		botonCalcular_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -148,6 +122,41 @@ public class VentanaAdjudicacion extends JPanel {
 				label_gananciaTotal.setText("Ganancia Total: $" + precioTotal);
 			}
 		});
+
+		JPanel panelInferiorDerecha = new JPanel();
+		panelInferior.add(panelInferiorDerecha);
+		
+		JButton botonMenu = new JButton("Volver al Menu");
+		botonMenu.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		botonMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pantallaPrincipal.cambiarVentana("Ventana Menu");
+			}
+		});
+		panelInferiorDerecha.setLayout(new BorderLayout(0, 0));
+		panelInferiorDerecha.add(botonMenu);
+		
+		JPanel panel_1 = new JPanel();
+		panelInferiorDerecha.add(panel_1, BorderLayout.NORTH);
+		panel_1.setPreferredSize(new Dimension(0,50));
+		
+		JPanel panel_2 = new JPanel();
+		panelInferiorDerecha.add(panel_2, BorderLayout.SOUTH);
+		panel_2.setPreferredSize(new Dimension(0,15));
+		
+		JPanel panel = new JPanel();
+		panelInferiorDerecha.add(panel, BorderLayout.WEST);
+		panel.setPreferredSize(new Dimension(60,0));
+		
+		JPanel panel_3 = new JPanel();
+		panelInferiorDerecha.add(panel_3, BorderLayout.EAST);
+		panel_3.setPreferredSize(new Dimension(50,0));
+
+		JPanel panelIzquierda = new JPanel();
+		add(panelIzquierda, BorderLayout.WEST);
+
+		JPanel panelDerecha = new JPanel();
+		add(panelDerecha, BorderLayout.EAST);
 	}
 
 	public void actualizarTablaAdjudicacion(LinkedList<Reserva> reservas) {
